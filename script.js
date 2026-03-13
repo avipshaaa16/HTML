60158002ba5ae93795f82439add134160d07ee41
avipshaaa16 <avipshaneupane21@gmail.com>
2026-03-13 18:07:34 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-function myfunction() {
-    var a = 19;
-    document.getElementById("result").innerHTML = a + a;
+function myFunction() {
+    document.getElementById("result").innerHTML = 
+        document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML = 
+        document.getElementById("demo").childNodes[0].nodeValue;
 }
-function validate(e) {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const age = document.getElementById("age").value;
-    const msg = document.getElementById("message");
-    let message = "";
-    if (email === "") {
-        message = "Pls enter a correct email";
-        msg.style.color = "red";
-    }
-    else if (password === "") {
-        message = "Password must contain 8 characters";
-        msg.style.color = "red";
-    }
-    else if (age === "") {
-        message = "Age should be above 5";
-        msg.style.color = "red";
-    }
-    else {
-        message = "Login successful";
-        msg.style.color = "green";
-    }
-    msg.innerHTML = message;
-}
-document.getElementById("loginform").onsubmit = validate;
-document.getElementById("email").oninput = () => validate({ preventDefault: () => { } })
-document.getElementById("password").oninput = () => validate({ preventDefault: () => { } })
-document.getElementById("age").oninput = () => validate({ preventDefault: () => { } })
