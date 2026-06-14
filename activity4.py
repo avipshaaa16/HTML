5519c3dafe9018e31faecce23fca758bb637a908
avipshaaa16 <avipshaneupane21@gmail.com>
2026-06-14 12:28:17 +0545

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+birthday1 = "February 5th"
+birthday2 = "May 8th"
+birthday3 = "July 13th"
+birthday4 = "October 18th"
+birthday5 = "June 6th"
+
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
