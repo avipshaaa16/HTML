5666a9d80303f541d91f362f5764deab4ca2fa14
avipshaaa16 <avipshaneupane21@gmail.com>
2026-07-08 19:29:05 +0545

    Add files via upload

---
+++ activity18.py
@@ -0,0 +1,6 @@
+# Program to check if a character is an alphabet
+char = input("Enter a character: ")
+if char.isalpha():
+    print("The character is an alphabet.")
+else:
+    print("The character is NOT an alphabet.")
