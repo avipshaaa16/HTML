c92059f6c83cbd17b328cc78d22628470bd3b408
avipshaaa16 <avipshaneupane21@gmail.com>
2026-09-01 19:30:28 +0545

    Add files via upload

---
+++ activity57.py
@@ -0,0 +1,16 @@
+#Creating an empty list
+empty_list = []
+print()
+
+#creating numbers list
+number = [1,2,3,4,5]
+print(number)
+
+#Using * operator
+triples = [1,2,3] * 3
+print(triples)
+
+#reverse the given list
+alist = [100,200,300,400,500]
+alist = alist[::-1]
+print(alist, "\n")
